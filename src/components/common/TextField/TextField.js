d62a942504a4990be5b78ce51946b5c7b5a47436
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-05-14 17:34:05 +0200

    Change ReactDOM.render to root.render

--- src/components/common/TextField/TextField.js
+++ src/components/common/TextField/TextField.js
@@ -3,7 +3,7 @@ import styles from './TextField.module.scss';
 const TextField = props => {
 
   return (
-    <input type={ props.type } placeholder={ props.placeholder } className={ styles.TextField } />
+    <input type={ props.type } placeholder={ props.placeholder } className={ styles.TextField } name={ props.name || '' } autoComplete='on' />
   );
 
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import store from './redux/store'
 import App from './App';
 import { Provider } from 'react-redux';
@@ -7,13 +7,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles/normalize.scss';
 import './styles/global.scss';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.querySelector('#root')
+  </React.StrictMode>
 );
