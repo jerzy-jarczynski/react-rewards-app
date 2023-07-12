fabadb3919fb243913b16efa6c0402edefd17b5c
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-12 23:25:10 +0200

    Run server. Problem with client

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' ?  '/api' : 'http://localhost:3131/api';
+export const API_URL = (process.env.NODE_ENV === 'production') ? '/api' : 'http://localhost:8000/api';
